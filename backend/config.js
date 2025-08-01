b021616301a7ebde91e44d44ed02e4581e20064a
MadhuAerpula <164339651+MadhuAerpula@users.noreply.github.com>
2025-08-01 12:10:10 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,8 @@
-// backend/config.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -15,4 +14,4 @@ const connectDB = async () => {
   }
 };
 
-export default connectDB;
+module.exports = connectDB;
