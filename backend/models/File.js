c7cd25ad9ae41e126f15e11c6b764f4c10c86263
MadhuAerpula <164339651+MadhuAerpula@users.noreply.github.com>
2025-08-01 12:10:54 +0530

    Update File.js

--- backend/models/File.js
+++ backend/models/File.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const fileSchema = new mongoose.Schema({
   filename: String,
+  originalname: String,
+  mimetype: String,
+  size: Number,
+  // Add fileUrl and qrCodeUrl if you are saving them
   fileUrl: String,
   qrCodeUrl: String,
 });
